af5b3c7459210a7152c4750ee19c339e606f7b7c
Joe Nace <jwnace@gmail.com>
2022-11-29 17:58:36 -0500

    added solution for day 5 part 1

--- src/aoc-2017-csharp/Day05/Day05.cs
+++ src/aoc-2017-csharp/Day05/Day05.cs
@@ -2,9 +2,23 @@
 
 public static class Day05
 {
-    private static readonly string[] Input = File.ReadAllLines("Day05/day05.txt");
+    private static readonly int[] Input = File.ReadAllLines("Day05/day05.txt").Select(int.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        var steps = 0;
+        var i = 0;
+        
+        while (i >= 0 && i < Input.Length)
+        {
+            var temp = Input[i];
+            Input[i]++;
+            i += temp;
+            steps++;
+        }
+
+        return steps;
+    }
 
     public static int Part2() => 2;
 }
