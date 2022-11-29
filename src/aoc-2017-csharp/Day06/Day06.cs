b6ced422108f6b82b08a9f54c849bb83124656b8
Joe Nace <jwnace@gmail.com>
2022-11-29 18:10:43 -0500

    added placeholder for day 6 solution

---
+++ src/aoc-2017-csharp/Day06/Day06.cs
@@ -0,0 +1,10 @@
+namespace aoc_2017_csharp.Day06;
+
+public static class Day06
+{
+    public static string Input = File.ReadAllText("Day06/day06.txt");
+    
+    public static int Part1() => 1;
+    
+    public static int Part2() => 2;
+}
--- src/aoc-2017-csharp/GlobalUsings.cs
+++ src/aoc-2017-csharp/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using aoc_2017_csharp.Day02;
 global using aoc_2017_csharp.Day03;
 global using aoc_2017_csharp.Day04;
 global using aoc_2017_csharp.Day05;
+global using aoc_2017_csharp.Day06;
