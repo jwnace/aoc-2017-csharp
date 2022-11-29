1dd34dc45dfeda36ffbb0a47be636d425123f6d5
Joe Nace <jwnace@gmail.com>
2022-11-29 18:02:35 -0500

    added solution for day 5 part 2

--- src/aoc-2017-csharp/Day05/Day05.cs
+++ src/aoc-2017-csharp/Day05/Day05.cs
@@ -2,23 +2,39 @@
 
 public static class Day05
 {
-    private static readonly int[] Input = File.ReadAllLines("Day05/day05.txt").Select(int.Parse).ToArray();
+    private static readonly IEnumerable<int> Input = File.ReadAllLines("Day05/day05.txt").Select(int.Parse);
 
     public static int Part1()
     {
+        var offsets = Input.ToArray();   
         var steps = 0;
         var i = 0;
         
-        while (i >= 0 && i < Input.Length)
+        while (i >= 0 && i < offsets.Length)
         {
-            var temp = Input[i];
-            Input[i]++;
-            i += temp;
+            var offset = offsets[i];
+            offsets[i]++;
+            i += offset;
             steps++;
         }
 
         return steps;
     }
 
-    public static int Part2() => 2;
+    public static int Part2()
+    {
+        var offsets = Input.ToArray();
+        var steps = 0;
+        var i = 0;
+        
+        while (i >= 0 && i < offsets.Length)
+        {
+            var offset = offsets[i];
+            offsets[i] += offset >= 3 ? -1 : 1;
+            i += offset;
+            steps++;
+        }
+
+        return steps;
+    }
 }
