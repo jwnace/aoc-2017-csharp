335b73385927db6b825f854677b7df660e2d3249
Joe Nace <jwnace@gmail.com>
2022-11-29 00:08:10 -0500

    added solution for day 4

---
+++ src/aoc-2017-csharp/Day04/Day04.cs
@@ -0,0 +1,22 @@
+﻿namespace aoc_2017_csharp.Day04;
+
+public static class Day04
+{
+    private static readonly string[] Input = File.ReadAllLines("Day04/day04.txt");
+
+    public static int Part1() => Input.Count(AllWordsAreUnique);
+
+    public static int Part2() => Input.Count(x => AllWordsAreUnique(x) && ContainsNoAnagrams(x));
+
+    public static bool AllWordsAreUnique(string passphrase) =>
+        passphrase.Split(' ').GroupBy(x => x).Select(g => new { g.Key, Count = g.Count() }).All(g => g.Count == 1);
+
+    public static bool ContainsNoAnagrams(string passphrase)
+    {
+        var query = passphrase.Split(' ')
+            .Select(w => w.GroupBy(c => c).Select(g => new { g.Key, Count = g.Count() }).OrderBy(g => g.Key).ThenBy(g => g.Count).ToList())
+            .ToList();
+
+        return query.All(q => query.Count(x => x.SequenceEqual(q)) == 1);
+    }
+}
---
+++ tests/aoc-2017-csharp-tests/Day04Tests.cs
@@ -0,0 +1,32 @@
+﻿using aoc_2017_csharp.Day04;
+
+namespace aoc_2017_csharp_tests;
+
+public class Day04Tests
+{
+    [TestCase("aa bb cc dd ee", true)]
+    [TestCase("aa bb cc dd aa", false)]
+    [TestCase("aa bb cc dd aaa", true)]
+    public void AllWordsAreUniqueTest(string input, bool expected)
+    {
+        // act
+        var actual = Day04.AllWordsAreUnique(input);
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
+    [TestCase("abcde fghij", true)]
+    [TestCase("abcde xyz ecdab", false)]
+    [TestCase("a ab abc abd abf abj", true)]
+    [TestCase("iiii oiii ooii oooi oooo", true)]
+    [TestCase("oiii ioii iioi iiio", false)]
+    public void ContainsNoAnagramsTest(string input, bool expected)
+    {
+        // act
+        var actual = Day04.ContainsNoAnagrams(input);
+
+        // assert
+        actual.Should().Be(expected);
+    }
+}
