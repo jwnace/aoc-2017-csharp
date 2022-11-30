eee42cea893f891c56d852ac4a58e6eee2d7f536
Joe Nace <jwnace@gmail.com>
2022-11-29 20:22:14 -0500

    refactored solution for day 6

--- src/aoc-2017-csharp/Day06/Day06.cs
+++ src/aoc-2017-csharp/Day06/Day06.cs
@@ -2,29 +2,22 @@ namespace aoc_2017_csharp.Day06;
 
 public static class Day06
 {
-    public static string Input = File.ReadAllText("Day06/day06.txt");
+    private static readonly string Input = File.ReadAllText("Day06/day06.txt");
 
-    public static int Part1() => SolvePart1();
+    public static int Part1() => Solve(1);
 
-    public static int Part2() => SolvePart2();
+    public static int Part2() => Solve(2);
 
-    private static int SolvePart1()
+    private static int Solve(int part)
     {
         var cycles = 0;
         var states = new List<int[]>();
-        var state = Input.Split('\t').Select(int.Parse).ToArray();
+        var state = part == 1 ? Input.Split('\t').Select(int.Parse).ToArray() : GetRecurringState();
         states.Add(state.ToArray());
 
         while (true)
         {
-            var bank = state.Select((x, i) => new { Index = i, Blocks = x }).First(x => x.Blocks == state.Max());
-            state[bank.Index] = 0;
-
-            for (var i = 1; i <= bank.Blocks; i++)
-            {
-                state[(bank.Index + i) % state.Length]++;
-            }
-
+            Redistribute(state);
             cycles++;
 
             if (states.Any(x => x.SequenceEqual(state)))
@@ -36,53 +29,26 @@ public static class Day06
         }
     }
 
-    private static int SolvePart2()
+    private static void Redistribute(int[] state)
     {
-        var cycles = 0;
-        var states = new List<int[]>();
-        var state = GetRecurringState();
-        states.Add(state.ToArray());
+        var bank = state.Select((x, i) => new { Index = i, Blocks = x }).First(x => x.Blocks == state.Max());
+        state[bank.Index] = 0;
 
-        while (true)
+        for (var i = 1; i <= bank.Blocks; i++)
         {
-            var bank = state.Select((x, i) => new { Index = i, Blocks = x }).First(x => x.Blocks == state.Max());
-            state[bank.Index] = 0;
-
-            for (var i = 1; i <= bank.Blocks; i++)
-            {
-                state[(bank.Index + i) % state.Length]++;
-            }
-
-            cycles++;
-
-            if (states.Any(x => x.SequenceEqual(state)))
-            {
-                return cycles;
-            }
-
-            states.Add(state.ToArray());
+            state[(bank.Index + i) % state.Length]++;
         }
     }
 
     private static int[] GetRecurringState()
     {
-        var cycles = 0;
         var states = new List<int[]>();
         var state = Input.Split('\t').Select(int.Parse).ToArray();
         states.Add(state.ToArray());
 
         while (true)
         {
-            var bank = state.Select((x, i) => new { Index = i, Blocks = x }).First(x => x.Blocks == state.Max());
-            state[bank.Index] = 0;
-
-            for (var i = 1; i <= bank.Blocks; i++)
-            {
-                state[(bank.Index + i) % state.Length]++;
-            }
-
-            cycles++;
-
+            Redistribute(state);
             states.Add(state.ToArray());
 
             if (states.Count(x => x.SequenceEqual(state)) > 1)
