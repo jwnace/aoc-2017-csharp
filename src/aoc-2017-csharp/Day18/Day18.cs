572b0028494eb410667e3a359abb5e67f2e8f3a0
Joe Nace <jwnace@gmail.com>
2022-12-29 00:05:55 -0500

    added solution for day 18

--- src/aoc-2017-csharp/Day18/Day18.cs
+++ src/aoc-2017-csharp/Day18/Day18.cs
@@ -4,7 +4,134 @@ public static class Day18
 {
     private static readonly string[] Input = File.ReadAllLines("Day18/day18.txt");
 
-    public static int Part1() => 1;
+    public static long Part1()
+    {
+        var lastSound = 0L;
+        var registers = new Dictionary<string, long>
+        {
+            { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 }, { "i", 0 },
+            { "j", 0 }, { "k", 0 }, { "l", 0 }, { "m", 0 }, { "n", 0 }, { "o", 0 }, { "p", 0 }, { "q", 0 }, { "r", 0 },
+            { "s", 0 }, { "t", 0 }, { "u", 0 }, { "v", 0 }, { "w", 0 }, { "x", 0 }, { "y", 0 }, { "z", 0 },
+        };
 
-    public static int Part2() => 2;
+        for (var i = 0L; i < Input.Length; i++)
+        {
+            var line = Input[i];
+            var values = line.Split(' ');
+            var instruction = values[0];
+            var x = values[1];
+            var y = values.Length == 3 ? values[2] : "0";
+            var xValue = long.TryParse(x, out var vx) ? vx : registers[x];
+            var yValue = long.TryParse(y, out var vy) ? vy : registers[y];
+
+            switch (instruction)
+            {
+                case "snd":
+                    lastSound = xValue;
+                    break;
+                case "set":
+                    registers[x] = yValue;
+                    break;
+                case "add":
+                    registers[x] += yValue;
+                    break;
+                case "mul":
+                    registers[x] *= yValue;
+                    break;
+                case "mod":
+                    registers[x] %= yValue;
+                    break;
+                case "rcv" when xValue != 0L:
+                    return lastSound;
+                case "jgz" when xValue > 0L:
+                    i += yValue - 1;
+                    break;
+            }
+        }
+
+        throw new Exception("couldn't find a solution!");
+    }
+
+    public static int Part2()
+    {
+        var programA = new Program(0);
+        var programB = new Program(1);
+
+        while (true)
+        {
+            programA.Run(programB);
+            programB.Run(programA);
+
+            if (programA.IsWaitingToReceiveInput && programB.IsWaitingToReceiveInput)
+            {
+                break;
+            }
+        }
+
+        return programB.ValuesSent;
+    }
+
+    private class Program
+    {
+        private long _currentInstruction;
+        private readonly Dictionary<string, long> _registers = new()
+        {
+            { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 }, { "i", 0 },
+            { "j", 0 }, { "k", 0 }, { "l", 0 }, { "m", 0 }, { "n", 0 }, { "o", 0 }, { "p", 0 }, { "q", 0 }, { "r", 0 },
+            { "s", 0 }, { "t", 0 }, { "u", 0 }, { "v", 0 }, { "w", 0 }, { "x", 0 }, { "y", 0 }, { "z", 0 },
+        };
+
+        public Queue<long> MessageQueue { get; } = new();
+        public bool IsWaitingToReceiveInput { get; private set; }
+        public int ValuesSent { get; private set; }
+
+        public Program(int id)
+        {
+            _registers["p"] = id;
+        }
+
+        public void Run(Program otherProgram)
+        {
+            var line = Input[_currentInstruction];
+            var values = line.Split(' ');
+            var instruction = values[0];
+            var x = values[1];
+            var y = values.Length == 3 ? values[2] : "0";
+            var xValue = long.TryParse(x, out var vx) ? vx : _registers[x];
+            var yValue = long.TryParse(y, out var vy) ? vy : _registers[y];
+
+            switch (instruction)
+            {
+                case "snd":
+                    otherProgram.MessageQueue.Enqueue(xValue);
+                    ValuesSent++;
+                    break;
+                case "set":
+                    _registers[x] = yValue;
+                    break;
+                case "add":
+                    _registers[x] += yValue;
+                    break;
+                case "mul":
+                    _registers[x] *= yValue;
+                    break;
+                case "mod":
+                    _registers[x] %= yValue;
+                    break;
+                case "rcv" when MessageQueue.Any():
+                    IsWaitingToReceiveInput = false;
+                    _registers[x] = MessageQueue.Dequeue();
+                    break;
+                case "rcv" when !MessageQueue.Any():
+                    IsWaitingToReceiveInput = true;
+                    _currentInstruction--;
+                    break;
+                case "jgz" when xValue > 0L:
+                    _currentInstruction += yValue - 1;
+                    break;
+            }
+
+            _currentInstruction++;
+        }
+    }
 }
--- tests/aoc-2017-csharp-tests/Day18Tests.cs
+++ tests/aoc-2017-csharp-tests/Day18Tests.cs
@@ -7,7 +7,7 @@ public class Day18Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2_951;
         var actual = Day18.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +15,7 @@ public class Day18Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 7_366;
         var actual = Day18.Part2();
         actual.Should().Be(expected);
     }
