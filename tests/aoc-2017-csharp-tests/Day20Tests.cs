7a043b423c7a08b4c5c2fc2e556a7560723106c6
Joe Nace <jwnace@gmail.com>
2022-12-29 17:15:25 -0500

    added solution for day 20

--- tests/aoc-2017-csharp-tests/Day20Tests.cs
+++ tests/aoc-2017-csharp-tests/Day20Tests.cs
@@ -7,7 +7,7 @@ public class Day20Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 300;
         var actual = Day20.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +15,7 @@ public class Day20Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 502;
         var actual = Day20.Part2();
         actual.Should().Be(expected);
     }
