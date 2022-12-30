382563a771c7a9662cc1668ab3cbc761565778bf
Joe Nace <jwnace@gmail.com>
2022-12-29 23:27:43 -0500

    updated tests for day 21

--- tests/aoc-2017-csharp-tests/Day21Tests.cs
+++ tests/aoc-2017-csharp-tests/Day21Tests.cs
@@ -7,7 +7,7 @@ public class Day21Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 155;
         var actual = Day21.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +15,7 @@ public class Day21Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2_449_665;
         var actual = Day21.Part2();
         actual.Should().Be(expected);
     }
