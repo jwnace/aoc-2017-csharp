c412de25b9defb7c18509df4b4eb818b20faa3c0
Joe Nace <jwnace@gmail.com>
2022-12-30 13:02:49 -0500

    lots of refactoring and cleanup to drastically improve run-time

--- src/aoc-2017-csharp/Day23/Day23.cs
+++ src/aoc-2017-csharp/Day23/Day23.cs
@@ -4,25 +4,23 @@ public static class Day23
 {
     private static readonly string[] Input = File.ReadAllLines("Day23/day23.txt");
 
-    public static long Part1()
+    public static int Part1()
     {
         var result = 0;
-        var registers = new Dictionary<string, long>
+        var registers = new Dictionary<string, int>
         {
-            { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 }, { "i", 0 },
-            { "j", 0 }, { "k", 0 }, { "l", 0 }, { "m", 0 }, { "n", 0 }, { "o", 0 }, { "p", 0 }, { "q", 0 }, { "r", 0 },
-            { "s", 0 }, { "t", 0 }, { "u", 0 }, { "v", 0 }, { "w", 0 }, { "x", 0 }, { "y", 0 }, { "z", 0 },
+            { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 },
         };
 
-        for (var i = 0L; i < Input.Length; i++)
+        for (var i = 0; i < Input.Length; i++)
         {
             var line = Input[i];
             var values = line.Split(' ');
             var instruction = values[0];
             var x = values[1];
             var y = values.Length == 3 ? values[2] : "0";
-            var xValue = long.TryParse(x, out var vx) ? vx : registers[x];
-            var yValue = long.TryParse(y, out var vy) ? vy : registers[y];
+            var xValue = int.TryParse(x, out var vx) ? vx : registers[x];
+            var yValue = int.TryParse(y, out var vy) ? vy : registers[y];
 
             switch (instruction)
             {
@@ -45,56 +43,32 @@ public static class Day23
         return result;
     }
 
-    public static long Part2() => RunOptimizedProgram();
+    public static int Part2() => CountCompositeNumbers();
 
-    private static long RunOptimizedProgram()
+    private static int CountCompositeNumbers()
     {
-        ulong step = 0;
-        long b, c, d, e, f, h = 0;
+        var start = 109900;
+        var end = start + 17_000;
+        var count = 0;
 
-        b = 99 * 100 + 100_000;
-        c = b + 17_000;
-
-        while (true)
+        for (var i = start; i <= end; i += 17)
         {
-            f = 1;
-            d = 2;
-
-            do
-            {
-                if (step % 1_000 == 0)
-                {
-                    Console.WriteLine(step);
-                }
-
-                e = 2;
-
-                do
-                {
-                    if (d * e == b)
-                    {
-                        f = 0;
-                    }
-
-                    e++;
-                } while (e != b);
-
-                d++;
-
-                step++;
-            } while (d != b);
-
-            if (f == 0)
-            {
-                h++;
-            }
-
-            if (b == c)
-            {
-                return h;
-            }
-
-            b += 17;
+            count += IsPrime(i) ? 0 : 1;
         }
+
+        return count;
+    }
+
+    private static bool IsPrime(int number)
+    {
+        for (var i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
