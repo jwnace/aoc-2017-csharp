96e6ba49f6a870a0aa2812d534e24d9d0e32af91
Joe Nace <jwnace@gmail.com>
2022-12-31 14:52:42 -0500

    WIP day 24

--- src/aoc-2017-csharp/Day24/Day24.cs
+++ src/aoc-2017-csharp/Day24/Day24.cs
@@ -1,10 +1,47 @@
-﻿namespace aoc_2017_csharp.Day24;
+﻿using aoc_2017_csharp.Extensions;
+
+namespace aoc_2017_csharp.Day24;
 
 public static class Day24
 {
     private static readonly string[] Input = File.ReadAllLines("Day24/day24.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        // the first port you use must be of type 0
+
+        // It doesn't matter what type of port you end with; your goal is just to make the bridge as strong as possible.
+
+        // The strength of a bridge is the sum of the port types in each component. For example, if your bridge is made
+        // of components 0/3, 3/7, and 7/4, your bridge has a strength of 0+3 + 3+7 + 7+4 = 24.
+
+        var components = GetComponents();
+
+        // var combinations = GetCombinations(components);
+
+        // for (int i = 0; i < components.Count - 1; i++)
+        // {
+        //     for (int j = i + 1; j < components.Count; j++)
+        //     {
+        //
+        //     }
+        // }
+
+        return 1;
+    }
+
+    private static List<(int, int)> GetComponents()
+    {
+        var components = new List<(int, int)>();
+
+        foreach (var line in Input)
+        {
+            var values = line.Split('/').Select(int.Parse).ToArray();
+            components.Add((values[0], values[1]));
+        }
+
+        return components;
+    }
 
     public static int Part2() => 2;
 }
